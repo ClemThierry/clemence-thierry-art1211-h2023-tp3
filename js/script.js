d4d9605348f9cb546a7be56288b20f9ff42bde0c
Clemence <clemence.thierry60@orange.fr>
2023-04-30 19:00:29 +0200

    Début de la 3e page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-let toggle = document.querySelector('.toggle');
-let body = document.querySelector('body');
+let openMenuButton = document.querySelector(".open");
+let closeMenuButton = document.querySelector(".close");
 
-toggle.addEventListener('click', function() {
-    body.classList.toggle('open');
+
+openMenuButton.addEventListener("click", () => {
+    closeMenuButton.classList.remove("hide");
+    document.querySelector(".menu").classList.add("menu-open");
+    openMenuButton.classList.add("hide");
+})
+
+closeMenuButton.addEventListener("click", () => {
+    openMenuButton.classList.remove("hide");
+    document.querySelector(".menu").classList.remove("menu-open");
+    closeMenuButton.classList.add("hide");
 })
